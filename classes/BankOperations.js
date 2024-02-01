8998ff721afc62afb893e270f52262f7c1419a43
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-01 22:58:18 +0600

    Chore: Completed create account function with new account logic

--- classes/BankOperations.js
+++ classes/BankOperations.js
@@ -13,5 +13,11 @@ class BankOperations {
       console.log(`Account Number ${number} already exists!`);
       return;
     }
+
+    const newAccount = new BankAccount(name, number, type, initialDeposit);
+    console.log("Processing your details...");
+    console.log("Please Wait...");
+    this.accounts.push(newAccount);
+    console.log("Your account has been created successfully!");
   }
 }
